71ac4512bcce4ff0e8944107c6a28b92a9b42450
Maxim228227 <max20050307@gmail.com>
2025-09-09 21:03:09 +0300

    Новый класс

---
+++ Calculator/per/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.per
+{
+    internal class Product
+    {
+        public int Id { get; set; }
+        public void Productt()
+        {
+            int i = 0;
+        }
+    }
+}
